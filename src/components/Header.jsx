d43d0aba165e0501e04a00bb8a073e8b850023cd
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-15 12:03:21 +0200

    setup header menu on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { RiMenu3Line, RiSearchLine } from "react-icons/ri"
 import { BsThreeDots } from "react-icons/bs"
-import NewsTypeListItem from "./NewsTypeListItem"
+import { RxCross2 } from "react-icons/rx"
 import { nanoid } from "nanoid"
 import { useEffect, useState } from "react"
 import { useGetLatestNewsByCategoryQuery } from "../features/newsApi"
 import { useDispatch } from "react-redux"
 import { setLatestNews } from "../features/news"
+import NewsTypeListItem from "./NewsTypeListItem"
 
 const headerMenu = [
   "general",
@@ -47,49 +48,79 @@ const Header = () => {
   }
 
   return (
-    <div className="w-full px-5 xl:px-0 flex gap-10 justify-between py-7 font-medium items-center sticky top-0 z-50 bg-white backdrop-blur-md">
-      <div className="font-extrabold text-4xl xl:text-5xl">
-        NGlobal
-      </div>
-      <ul className="xl:flex gap-20 text-xl hidden items-center">
-        {headerMenu.map((menu) => (
-          <NewsTypeListItem
-            key={nanoid()}
-            activeCategory={currentNewsCategory}
+    <div className="sticky top-0 z-50">
+      <div className="w-full px-5 xl:px-0 flex gap-10 justify-between py-7 font-medium items-center bg-white backdrop-blur-md">
+        <div className="font-extrabold text-4xl xl:text-5xl bg-white">
+          NGlobal
+        </div>
+        <ul className="xl:flex gap-20 text-xl hidden items-center">
+          {headerMenu.map((menu) => (
+            <NewsTypeListItem
+              key={nanoid()}
+              activeCategory={currentNewsCategory}
+              onClick={() =>
+                setCurrentNewsCategory(menu.toLocaleLowerCase())
+              }
+            >
+              {menu}
+            </NewsTypeListItem>
+          ))}
+          <li
+            className="cursor-pointer"
             onClick={() =>
-              setCurrentNewsCategory(menu.toLocaleLowerCase())
+              setIsAdditionalMenusOpened((prev) => !prev)
             }
           >
-            {menu}
-          </NewsTypeListItem>
-        ))}
-        <li
-          className="cursor-pointer"
-          onClick={() => setIsAdditionalMenusOpened((prev) => !prev)}
-        >
-          <BsThreeDots />
-        </li>
-      </ul>
-      <div className="flex items-center gap-5">
-        <RiSearchLine className="h-6 w-6" />
-        <RiMenu3Line className="h-6 w-6 xl:hidden block" />
+            <BsThreeDots />
+          </li>
+        </ul>
+        <div className="flex items-center gap-5">
+          <RiSearchLine className="h-6 w-6" />
+          <button
+            onClick={() =>
+              setIsAdditionalMenusOpened((prev) => !prev)
+            }
+          >
+            {isAdditionalMenusOpened ? (
+              <RxCross2 className="h-6 w-6 xl:hidden block" />
+            ) : (
+              <RiMenu3Line className="h-6 w-6 xl:hidden block" />
+            )}
+          </button>
+        </div>
       </div>
-      <ul
-        className={`absolute top-20 left-0 -z-20 transition-all duration-500 right-0 w-full grid grid-cols-1 bg-white divide-y shadow-lg px-5 rounded-b-xl ${
+      <div
+        className={`absolute top-24 -z-10 left-0 transition-all duration-500 ease-out right-0 w-full bg-white shadow-lg px-5 rounded-b-xl ${
           isAdditionalMenusOpened
             ? "translate-y-0"
-            : "-translate-y-[1000px]"
+            : "-translate-y-[50rem]"
         }`}
       >
-        {additionalHeaderMenu.map((menu) => (
-          <NewsTypeListItem
-            onClick={() => onSelectAddtionalHeaderMenu(menu)}
-            key={nanoid()}
-          >
-            <span className="py-5">{menu}</span>
-          </NewsTypeListItem>
-        ))}
-      </ul>
+        <div className="hidden md:grid grid-cols-1 divide-y">
+          {additionalHeaderMenu.map((menu) => (
+            <NewsTypeListItem
+              onClick={() => onSelectAddtionalHeaderMenu(menu)}
+              key={nanoid()}
+            >
+              <span className="py-5">{menu}</span>
+            </NewsTypeListItem>
+          ))}
+        </div>
+        <div className="grid md:hidden grid-cols-1 divide-y">
+          {headerMenu.concat(additionalHeaderMenu).map((menu) => (
+            <NewsTypeListItem
+              activeCategory={currentNewsCategory}
+              onClick={() => {
+                setCurrentNewsCategory(menu)
+                setIsAdditionalMenusOpened(false)
+              }}
+              key={nanoid()}
+            >
+              <span className="py-5">{menu}</span>
+            </NewsTypeListItem>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
